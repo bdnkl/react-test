d6428dd458fcb2a75a3c86b81681854de9355dd9
bdnkl <boas_d@web.de>
2022-01-13 23:05:09 +0100

    Updates

---
+++ _next/static/S_UCziaRdT_Yxr4VJx97k/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1ec04c6caeef3502.js"],"/_error":["static/chunks/pages/_error-dcf0021c62d11903.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
